0e86524004b9aea9b6f1309525f621cb4ab70786
paulharte <hartepaul@gmail.com>
2024-04-25 10:40:34 +0100

    ISO Date fix

--- src/components/BlogHelmet/index.js
+++ src/components/BlogHelmet/index.js
@@ -5,7 +5,7 @@ import userConfig from '../../../config';
 import imgSrc from '../../main.png';
 
 function BlogHelmet({pageTitle, description, publishDate, slug}) {
-  const publishDateUtc = publishDate ? publishDate.toUTCString() : null;
+  const publishDateIso = publishDate ? publishDate.toISOString() : null;
   const url = slug ? userConfig.siteUrl + slug : userConfig.siteUrl
 
   return (
@@ -17,8 +17,8 @@ function BlogHelmet({pageTitle, description, publishDate, slug}) {
       {!slug && <meta property="og:type" content="article" />}
       <meta name="description" property="og:description" content={description}/>
       <meta name="author" property="article:author" content={userConfig.author}/>
-      {publishDateUtc && <meta name="published_time" property="article:published_time" content={publishDateUtc}/>}
-      {publishDateUtc && <meta name="modified_time" property="article:modified_time" content={publishDateUtc}/>}
+      {publishDateIso && <meta name="published_time" property="article:published_time" content={publishDateIso}/>}
+      {publishDateIso && <meta name="modified_time" property="article:modified_time" content={publishDateIso}/>}
       <meta property="article:tag" content='software development'/>
       <meta name="image" property="og:image" content={`${userConfig.siteUrl}${imgSrc}`}/>
       <meta name="url" property="og:url" content={url}/>
